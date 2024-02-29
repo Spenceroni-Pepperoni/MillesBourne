61b1eae00892a5f7eda2b562a67e8235577817b2
jasonchen595 <jchen8412@inst.hcpss.org>
2024-02-29 10:47:20 -0500

    Update Player.java
    added constructor, draw, getters, playCard, receiveHazard, speelLimit

--- MillesBourne/lib/src/main/java/_MillesBourne/Player.java
+++ MillesBourne/lib/src/main/java/_MillesBourne/Player.java
@@ -1,5 +1,50 @@
 package _MillesBourne;
+import java.util.*;
 
 public class Player {
-
+	private ArrayList<Card> deck;
+	private int mileage;
+	private boolean turn;
+	private ArrayList<Card> hazards;
+	
+	public Player() {
+		deck = new ArrayList<Card>();
+		mileage = 0;
+		turn = false;
+	}
+	
+	public void drawPile(Card draw) {
+		deck.add(draw);
+	}
+	
+	public void drawDiscard(Card draw) {
+		deck.add(draw);
+	}
+	
+	public int getMileage() {
+		return mileage;
+	}
+	
+	public boolean getTurn() {
+		return turn;
+	}
+	
+	public Card playCard(int index) {
+		Card temp = deck.get(index);
+		deck.remove(index);
+		return temp;
+	}
+	
+	public void receiveHazard(Card card) {
+		hazards.add(card);
+	}
+	
+	public boolean speedLimit() {
+		for(Card hazard : hazards) {
+			if(hazard.getName().equals("speedLimit")) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
