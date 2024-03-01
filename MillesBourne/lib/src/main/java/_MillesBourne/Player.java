98fd2cfb53922e29bfb0c55a368a2176731b341e
jasonchen595 <jchen8412@inst.hcpss.org>
2024-03-01 09:31:51 -0500

    Wrong class commited previously

--- MillesBourne/lib/src/main/java/_MillesBourne/Player.java
+++ MillesBourne/lib/src/main/java/_MillesBourne/Player.java
@@ -2,7 +2,8 @@ package _MillesBourne;
 import java.util.*;
 
 public class Player {
-	private ArrayList<Card> deck;
+	
+	protected ArrayList<Card> deck;
 	private int mileage;
 	private boolean turn;
 	private ArrayList<Card> hazards;
@@ -39,9 +40,24 @@ public class Player {
 		hazards.add(card);
 	}
 	
+	public Card discard(int index) {
+		Card temp = deck.get(index);
+		deck.remove(index);
+		return temp;
+	}
+	
 	public boolean speedLimit() {
 		for(Card hazard : hazards) {
 			if(hazard.getName().equals("speedLimit")) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean hasHazard() {
+		for(Card hazard : hazards) {
+			if(!hazard.getName().equals("speedLimit")) {
 				return true;
 			}
 		}
