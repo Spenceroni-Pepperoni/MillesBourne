e60d7fd75a5ca67a94e95993e3d9f44120fd6b7a
Spencer McGrath <Samcgrath2005@gmail.com>
2024-03-01 08:58:29 -0500

    Made Card Class

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,9 +1,21 @@
 public abstract class Card {
 
-  private String name;
+    private String cardName;
 
-  public Card(String name){
-    this.name = name;
-  }
-  
+    /**
+     * Constructor for Card Class
+     * @param name name of the Card
+     */
+    Card(String name){
+        this.cardName = name;
+    }
+
+    abstract boolean getCanPlay();
+
+    abstract String getCardType();
+
+    public String getCardName() {
+        return cardName;
+    }
+
 }
