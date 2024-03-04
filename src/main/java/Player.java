dfccb6b504c08928a17728b16fed24d4f5678220
prussalesi <161034850+prussalesi@users.noreply.github.com>
2024-03-04 09:37:40 -0500

    Update Player.java

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,44 +1,41 @@
-//-----------------------------------------------------------------------------
-// Player class: simulates a player in the MilesQuest game
-//-----------------------------------------------------------------------------
+import java.util.*;
+
 public class Player {
-	private int[] deck = new int[6];
-	private boolean isTurn;
+	private ArrayList<Card> deck;
+
+	protected ArrayList<Card> deck;
 	private int mileage;
-	private int deckSize = 0;
-	private String currentHazard;
-	
-	public Player() {
-		mileage = 0;
-	}
-	
-	public void drawPile() {
-		if (deckSize < 5) {
-			deck[deckSize] = Game.getPileCard(); //this method isn't in decomp but we might need it
-			deckSize++;
-		}
-	}
-	
-	public void drawDiscard() {
-		if (deckSize < 5) {
-			deck[deckSize] = Game.getDiscardCard(); //same for this one
-			deckSize++;
-		}
-	}
-	
-	public int getMileage() {
-		return mileage;
-	}
-	
-	public boolean playCard(int cardIndex) {
-		
-	}
-	
+	private boolean turn;
+	private ArrayList<Card> hazards;
+
 	public void receiveHazard(Card card) {
-		
+		hazards.add(card);
 	}
-	
-	public boolean getTurn() {
+
+	public Card discard(int index) {
+		Card temp = deck.get(index);
+		deck.remove(index);
+		return temp;
+	}
+
+	public boolean speedLimit() {
+		for(Card hazard : hazards) {
+			if(hazard.getName().equals("speedLimit")) {
+				return false;
+			}
+		}
+
+	}
+
+	public boolean hasHazard() {
+		for(Card hazard : hazards) {
+			if(!hazard.getName().equals("speedLimit")) {
+				return true;
+			}
+		}
+		return false;
+	}
+}	public boolean getTurn() {
 		return isTurn;
 	}
 	
