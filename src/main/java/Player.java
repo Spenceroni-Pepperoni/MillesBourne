930f5aaf83534f00cd4a4b2db345e79a0cada0da
prussalesi <161034850+prussalesi@users.noreply.github.com>
2024-03-04 09:30:38 -0500

    Update Player.java

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,35 +1,45 @@
-
+//-----------------------------------------------------------------------------
+// Player class: simulates a player in the MilesQuest game
+//-----------------------------------------------------------------------------
 public class Player {
 	private int[] deck = new int[6];
 	private boolean isTurn;
 	private int mileage;
+	private int deckSize = 0;
+	private String currentHazard;
 	
 	public Player() {
 		mileage = 0;
 	}
 	
 	public void drawPile() {
-		
+		if (deckSize < 5) {
+			deck[deckSize] = Game.getPileCard(); //this method isn't in decomp but we might need it
+			deckSize++;
+		}
 	}
 	
 	public void drawDiscard() {
-		
+		if (deckSize < 5) {
+			deck[deckSize] = Game.getDiscardCard(); //same for this one
+			deckSize++;
+		}
 	}
 	
 	public int getMileage() {
-		
+		return mileage;
 	}
 	
 	public boolean playCard(int cardIndex) {
 		
 	}
 	
-	public boolean receiveHazard(Card card) {
+	public void receiveHazard(Card card) {
 		
 	}
 	
 	public boolean getTurn() {
-		
+		return isTurn;
 	}
 	
 	public Card discard(int cardIndex) {
