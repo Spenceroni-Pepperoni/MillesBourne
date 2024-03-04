82779c06b42baa7b7aa5d63201cf87bba98edfff
prussalesi <161034850+prussalesi@users.noreply.github.com>
2024-03-04 09:11:57 -0500

    Update Player.java

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,6 +1,6 @@
 
 public class Player {
-	private int[] deck;
+	private int[] deck = new int[6];
 	private boolean isTurn;
 	private int mileage;
 	
@@ -8,5 +8,31 @@ public class Player {
 		mileage = 0;
 	}
 	
+	public void drawPile() {
+		
+	}
 	
+	public void drawDiscard() {
+		
+	}
+	
+	public int getMileage() {
+		
+	}
+	
+	public boolean playCard(int cardIndex) {
+		
+	}
+	
+	public boolean receiveHazard(Card card) {
+		
+	}
+	
+	public boolean getTurn() {
+		
+	}
+	
+	public Card discard(int cardIndex) {
+		
+	}
 }
