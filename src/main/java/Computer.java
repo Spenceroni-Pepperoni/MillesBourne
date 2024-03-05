f23f1d966fd4c8aa342872762dbea4f5d0c8cd98
jasonchen595 <jchen8412@inst.hcpss.org>
2024-03-05 09:01:21 -0500

    Created and started on computer class

---
+++ src/main/java/Computer.java
@@ -0,0 +1,34 @@
+
+public class Computer extends Player {
+
+
+    public Computer() {
+        super();
+    }
+
+
+    public void takeTurn() {
+        if (hasHazard() == false) {
+            if (speedLimit() == true) {
+                int playIndex = -1;
+                for (Card hand : deck) {
+                    if (hand.getCardType().equals("Remedy") || hand.getCardType().equals("Safety")) {
+
+                        int max = 0;
+                        for (int i = 0; i < deck.size(); i++) {
+                            if (deck.get(i).getCardType().equals("Safety")) {
+                                if (deck.get(i).canPlay()) {
+                                    playIndex = i;
+                                    break;
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+    }
+
+}
